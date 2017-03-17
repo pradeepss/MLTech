b9a6275942ddd582360427adca50144331236d0c
pradeep sharma <pradeeps@apple.com>
2017-03-16 21:48:57 -0700

    Updated files

--- AmericanBulls.py
+++ AmericanBulls.py
@@ -1,13 +1,19 @@
 import requests
 from bs4 import BeautifulSoup
-stock = 'NFLX'
-link = 'https://www.americanbulls.com/SignalPage.aspx?lang=en&Ticker=' + stock
-r = requests.get(link)
-soup = BeautifulSoup(r.content, "html.parser")
+import openpyxl
+# stocks = ['NFLX','AAPL','NVDA','BMY','TSLA', 'MCD', 'S']
+# stocks = 'S'
+stocks = ['DAL','AAL','UAL','LUV'] #Airlines
+
 # print(soup)
-for each_div in soup.findAll('span',{'class':'dxeBase vtoptext10a'}):
+
+for stock in stocks:
+    link = 'https://www.americanbulls.com/SignalPage.aspx?lang=en&Ticker=' + stock
+    r = requests.get(link)
+    soup = BeautifulSoup(r.content, "html.parser")
+    for each_div in soup.findAll('span',{'class':'dxeBase vtoptext10a'}):
 # for each_div in soup.findAll('td',{'class':'dxgv'}):
-    print(stock + " ===> " + each_div.text)
+        print(stock + " ===> " + each_div.text)
 
 
 
